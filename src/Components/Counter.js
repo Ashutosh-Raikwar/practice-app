57890395b90290874e05cc823b47a26d79c08c6d
ashutosh <ashutoshraikwar225@gmail.com>
2024-06-16 23:36:00 +0530

    added a simple counter

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 
 function Counter(){
-    const greet = () =>{
-        alert("hello guys");
+
+    const [count, setCount] = useState(0);
+    // const greet = () =>{
+    //     alert("hello guys");
+    // }
+
+
+    const handleIncrement = () =>{
+        console.log(count)
+        setCount(count+1);
+    }
+
+    
+    const handleDecrement = () =>{
+        setCount(count-1);
     }
 
     return(
         <div>
-            <button onClick={greet} >😎</button>
-            
+            <h2>Counter Component</h2>
+            <button onClick={handleIncrement} >➕</button>
+            <span>{count}</span>
+            <button onClick={handleDecrement} >➖</button>
         </div>
     )
 }
